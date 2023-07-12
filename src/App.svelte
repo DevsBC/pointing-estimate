281aca265d3f7a9e7f755030f8230d4b07181650
DevsBC <developersbac@gmail.com>
2023-07-12 16:55:19 -0600

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,46 @@
+<script>
+  import { Router, links, Route } from "svelte-routing";
+  import {
+    Header,
+    HeaderNav,
+    HeaderNavItem,
+    SideNav,
+    SideNavItems,
+    SideNavLink,
+    SkipToContent,
+    Content,
+    InlineNotification,
+  } from "carbon-components-svelte";
+
+  import Home from "./routes/Home.svelte";
+
+</script>
+
+<div use:links>
+  <Router url="">
+    <Header
+      company="WINDRVR"
+      platformName="Pointing Estimate"
+      expandedByDefault={false}
+    >
+      <div slot="skip-to-content">
+        <SkipToContent />
+      </div>
+
+      <HeaderNav>
+        <HeaderNavItem text="Home" href="/" />
+      </HeaderNav>
+    </Header>
+
+    <Content>
+      <Route path="/" component={Home} />
+      <Route let:location>
+        <InlineNotification
+          hideCloseButton
+          title="Error:"
+          subtitle={`No route found for ${location.pathname}`}
+        />
+      </Route>
+    </Content>
+  </Router>
+</div>
