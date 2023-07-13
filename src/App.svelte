8036fa21b990c9b2fb1d164ef479ce556e39047a
Co Satoru <com.satoru@gmail.com>
2023-07-13 14:21:44 -0600

    remove routing

--- src/App.svelte
+++ src/App.svelte
@@ -1,37 +1,23 @@
 <script>
   // @ts-nocheck
-  import { Router, links, Route } from "svelte-routing";
   import {
     Header,
     SkipToContent,
     Content,
-    InlineNotification,
   } from "carbon-components-svelte";
 
   import Home from "./pages/Home.svelte"; 
 </script>
 
-<div use:links>
-  <Router url="">
-    <Header
-      company="WINDRVR"
-      platformName="Pointing Estimate"
-      expandedByDefault={false}
-    >
-      <div slot="skip-to-content">
-        <SkipToContent />
-      </div>
-    </Header>
+<Header
+  company="WINDRVR"
+  platformName="Pointing Estimate"
+  expandedByDefault={false}>
+  <div slot="skip-to-content">
+    <SkipToContent />
+  </div>
+</Header>
 
-    <Content>
-      <Route path="/" component={Home} />
-      <Route let:location>
-        <InlineNotification
-          hideCloseButton
-          title="Error:"
-          subtitle={`No route found for ${location.pathname}`}
-        />
-      </Route>
-    </Content>
-  </Router>
-</div>
+<Content>
+  <Home/>
+</Content>
